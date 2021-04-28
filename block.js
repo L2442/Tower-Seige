c38efe8d45f786b27001f107b391d5bbcf2575ae
L2442 <76606422+L2442@users.noreply.github.com>
2021-04-28 18:47:17 +0530

    Add files via upload

--- block.js
+++ block.js
@@ -1,21 +1,37 @@
-class Block  {
-    constructor(x,y,width,height)  {
+class Block{
+    constructor(x,y,width,height) {
         var options={
             isStatic:false,
             restitution:0.4,
             friction:0.0
         }
+        this.visibility = 255;
         this.width = width;
         this.height = height;
         this.body = Bodies.rectangle(x,y,width,height,options);
-        
 
         World.add(world,this.body);
-
     }
-    display()  {
-        rectMode(CENTER);
-        fill("cyan");
-        rect(this.body.position.x,this.body.position.y,this.width,this.height);
-    }
+    display(){
+        if(this.body.speed < 9){
+           var angle = this.body.angle;
+           var pos = this.body.position;
+           push();
+           fill(255,0,0);
+           translate(pos.x, pos.y);
+           rotate(angle);
+           rectMode(CENTER);
+           rect(0,0,this.width, this.height);
+           pop();
+           }
+           else{
+             World.remove(world, this.body);
+             push();
+             fill(255,0,0,this.visibility);
+             noStroke();
+             
+             this.visibility = this.visibility-2;
+             pop();
+           }
+         }    
 }
